04ddb5445325546e2bb56e506339f24ff9f69e50
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-01-31 00:49:34 +0100

    factor out stuff

--- img_to_q3map.py
+++ img_to_q3map.py
@@ -1,31 +1,9 @@
 from PIL import Image
 import sys
-
-def preamble():
-	print("")
-	print("// entity 0")
-	print("{")
-	print('"classname" "worldspawn"')
-	
-def cube(p1, p2):
-	print("// brush 0")
-	print("{")
-	print("brushDef")
-	print("{")
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p1[0], p1[1], p1[2],  p2[0], p1[1], p2[2],  p2[0], p1[1], p1[2]))
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p2[0], p1[1], p1[2],  p2[0], p2[1], p2[2],  p2[0], p2[1], p1[2]))
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p2[0], p2[1], p1[2],  p1[0], p2[1], p2[2],  p1[0], p2[1], p1[2]))
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p1[0], p2[1], p1[2],  p1[0], p1[1], p2[2],  p1[0], p1[1], p1[2]))
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p1[0], p1[1], p2[2],  p2[0], p2[1], p2[2],  p2[0], p1[1], p2[2]))
-	print("( %s %s %s ) ( %s %s %s ) ( %s %s %s ) ( ( 0.0625 0 63.5 ) ( 0 0.0625 0 ) ) NULL 0 0 0" % (p1[0], p1[1], p1[2],  p2[0], p2[1], p1[2],  p1[0], p2[1], p1[2]))
-	print("}")
-	print("}")
-	
-def finish():
-    print("}")
+import map0
 
 if __name__ == "__main__":
-	preamble()
+	map0.preamble()
 	
 	i = Image.open(sys.argv[1])
 	
@@ -34,5 +12,5 @@ if __name__ == "__main__":
 	    for y in range(0, size[1]):
 	        pixel = i.getpixel((x,y))
 	        if pixel[0] < 128:
-	            cube([x,y,0], [x+1, y+1, 1])
-	finish()
+	            map0.cube([x,y,0], [x+1, y+1, 1])
+	map0.finish()
